aad9982306b2f86907f84af12f611ea8c02d9fb8
YUGrekov <grekov.yura@list.ru>
2023-11-13 13:39:57 +0500

    Добавлена новая таблица генерации USO

--- graphic_arts/sql_uso.py
+++ graphic_arts/sql_uso.py
@@ -7,6 +7,7 @@ from request_sql import RequestSQL
 from general_functions import General_functions
 
 T_HW = 'hardware'
+T_USO = 'uso'
 T_AI = 'ai'
 T_DI = 'di'
 TYPE = 'USO'
@@ -14,8 +15,8 @@ TYPE = 'USO'
 
 class USO():
     '''Заполнение таблицы.'''
-    def __init__(self):
-        # self.logsTextEdit = logtext
+    def __init__(self, logtext):
+        self.logsTextEdit = logtext
         self.request = RequestSQL()
         self.dop_function = General_functions()
 
@@ -25,24 +26,35 @@ class USO():
         for modul in modules:
             result = self.request.check_table(modul)
             if not result:
-                # self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Таблица
-                #                            {modul} отсутсвует''', 2)
-                print(f'''SQL. {TYPE}. Таблица {modul} отсутсвует. Добавь!''')
+                self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Таблица
+                                           {modul} отсутсвует''', 2)
                 return False
             else:
                 if not self.request.check_row_table(modul):
-                    # self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Таблица
-                    #                            {modul} пустая. Заполни!''', 2)
-                    print(f'SQL. {TYPE}. Таблица {modul} пустая. Заполни!')
+                    self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Таблица
+                                               {modul} пустая. Заполни!''', 2)
                     return False
+
+        result = self.request.check_table(T_USO)
+        if not result:
+            self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Таблица uso
+                                        отсутсвует''', 2)
+            self.request.new_table_orm(Model)
+            self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Таблица uso
+                                        добавлена''', 0)
         return True
 
-    def add_process(self, data, column, modul):
+    def add_process(self, data, column, modul, fl_all=False):
         if not len(data):
             return False
 
+        count = 0
         for signal in data:
-            self.entry_array[column] = f'{modul}[{signal.id}]'
+            if not fl_all:
+                self.entry_array[column] = f'{modul}[{signal.id}]'
+            else:
+                count += 1
+                self.entry_array[f'{column}{count}'] = f'{modul}[{signal.id}]'
         return True
 
     def ai_temp(self):
@@ -57,24 +69,36 @@ class USO():
                                         ((AI.tag % ('CST%')) | (AI.tag_eng % ('CST%')))),
                                        AI.name)
         if not self.add_process(temp, 'temperature', 'AI'):
-            print(f'SQL. {TYPE}. {self.uso.uso} - Температура не найдена')
-            # self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. {self.uso.uso} -
-            #                            Температура не найдена''', 2)
+            self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. {self.uso.uso} -
+                                       Температура не найдена''', 2)
 
-    def entry_to_base(self, signal, tag):
+    def di_door(self):
+        '''Поиск сигнала дверь шкафа открыта.'''
+        door = self.request.select_orm(DI,
+                                       ((DI.name % (f'%{self.uso.uso}%')) &
+                                        ((DI.name % ('%двер%')) | (DI.name % ('%Двер%'))) &
+                                        ((DI.tag % ('CSC%')) | (DI.tag_eng % ('CSC%')))),
+                                       DI.name)
+        if not self.add_process(door, 'door', 'DI'):
+            self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. {self.uso.uso} -
+                                       Сигнал "Двери открыты"
+                                       не найдено''', 2)
+
+    def di_all(self):
+        '''Поиск остальных сигналов диагностики шкафа.'''
+        all_di = self.request.select_orm(DI,
+                                         ((DI.name % (f'%{self.uso.uso}%')) &
+                                          (~((DI.name % ('%двер%')) | (DI.name % ('%Двер%')))) &
+                                          ((DI.tag % ('CSC%')) | (DI.tag_eng % ('CSC%')))),
+                                         DI.name)
+        if not self.add_process(all_di, 'signal_', 'DI', True):
+            self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. {self.uso.uso} -
+                                       Сигналы диагностики
+                                       шкафа не найдены''', 2)
+
+    def entry_to_base(self):
         '''Добавление нового сигнала.'''
-        num = f'0{signal.module}' if signal.module < 10 else f'{signal.module}'
-
-        list_RS = dict(id=self.count_row,
-                       variable=f'RS[{self.count_row}]',
-                       name=signal.description,
-                       pValue=f'{tag}_{num}.COM_CH[{signal.channel}]',
-                       uso=signal.uso,
-                       basket=signal.basket,
-                       module=signal.module,
-                       channel=signal.channel)
-
-        self.request.write_base_orm(list_RS, Model)
+        self.request.write_base_orm(self.entry_array, Model)
         self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Добавлен
                                    новый сигнал: {self.count_row}''', 0)
 
@@ -85,10 +109,9 @@ class USO():
                 raise
             # перед заполнением таблица должна быть пуста
             if self.request.count_row_orm(Model) > 0:
-                print(f'SQL. {TYPE}. Таблица заполнена. Перед заполнением необходимо очистить таблицу')
-                # self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Таблица заполнена.
-                #                            Перед заполнением необходимо
-                #                            очистить таблицу''', 2)
+                self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Таблица заполнена.
+                                           Перед заполнением необходимо
+                                           очистить таблицу''', 2)
                 raise
             data_uso = self.request.non_repea_names(HW, HW.uso, HW.uso)
             self.count_row = 0
@@ -97,19 +120,12 @@ class USO():
                 # Температура шкафа
                 self.ai_temp()
                 # Двери открыты
-                # self.di_door(uso.uso)
+                self.di_door()
                 # Остальные сигналы
-                # self.di_all(uso.uso)
-
-                # self.entry_to_base()
-
-            # self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Работа
-            #                            с таблицей завершена''', 1)
+                self.di_all()
+                self.entry_to_base()
+            self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Работа
+                                       с таблицей завершена''', 1)
         except Exception:
-            print(traceback.format_exc())
-            # self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Ошибка
-            #                            {traceback.format_exc()}''', 2)
-
-
-a = USO()
-a.add_uso()
+            self.logsTextEdit.logs_msg(f'''SQL. {TYPE}. Ошибка
+                                       {traceback.format_exc()}''', 2)
